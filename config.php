d9d9bc35a12f7a6d18c0aeb3dce36e1939d81eb3
Jan Chlouba <cboubik@gmail.com>
2019-03-19 20:38:13 +0100

    v0.2

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+    'servername' => 'https://bravdf.bakalari.cz:444/bakaweb',               // address for baklaře
+    'username' => '',                                                       // user name
+    'password' => ''                                                        // password
+);
+ 
