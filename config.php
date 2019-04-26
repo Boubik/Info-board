585fa6d6fc7f00c48e43a31772f836d83b661ab8
Jan Chlouba <cboubik@gmail.com>
2019-04-26 19:20:42 +0200

    add logs to get_rozvrh

--- config.php
+++ config.php
@@ -19,5 +19,9 @@ return array(
 
     //cantina
     'auto_refresh' => false,                                                    // auto restart
+
+    //logs
+    'log' => true,                                                              // if true it will create logs in folder logs
+    'delete_log' => 7,                                                          // after x (default 7) day will delete logs (last log will be x days old)
 );
  
