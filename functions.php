4d6f3e9c912c966ccb33768b098d1b9c2fc9d269
Jan Chlouba <cboubik@gmail.com>
2019-06-14 09:26:25 +0200

    better css, fix code after update, working "ajax"

--- functions.php
+++ functions.php
@@ -130,3 +130,110 @@ function full_rozvrh(){
     }
     return $rozvrh;
 }
+
+function cantina()
+{
+    $xml = @simplexml_load_file("jidelnicek.xml");
+    if (!$xml) {
+        echo "Nejsou data z jidelnicek.xml";
+    }
+    $jidelak = array();
+    foreach ($xml->jidlo as $jidlo) {
+        if ((String)$jidlo->datum == date("Y-m-d")) {
+            $jidelak[strval($jidlo->datum)][strval($jidlo->druh)] = strval($jidlo->nazev);
+        }
+    }
+
+    echo "<div class=\"cantina_container\"><div class=\"cantina\">";
+    foreach ($jidelak as $den) {
+        $datum = array_search($den, $jidelak);
+        echo "<div class=\"den\"><div class=\"datum\">" . $datum . "</div>";
+        foreach ($den as $jidlo) {
+            $key = array_search($jidlo, $den);
+            if (substr($key, 0, 4) != "bal.") {
+                echo "<div class=\"jidlo\">" . $key . "</div> <div class=\"jidlo_popis\">" . $jidlo . "</div>";
+            }
+        }
+        echo "</div><br>";
+    }
+    echo "</div></div>";
+}
+
+function news()
+{
+    $configs = include('config.php');
+    $url = $configs["news_url"];
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+    $result = curl_exec($ch);
+    $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);   //get status code
+    curl_close($ch);
+
+    if ($status_code == 200) {
+        $xml = new SimpleXMLElement($result);
+        echo "<div class=\"news_container\"><div class=\"news\">";
+        $x = 0;
+        foreach ($xml->channel->item as $item) { 
+            echo "<div class=\"new\">";
+            echo "<div class=\"title\">".$item->title."</div>";
+            echo "<div class=\"pubDate\">".substr((String)$item->pubDate, 0, 16)."</div>";
+            echo "</div>";
+            $x++;
+            if($x == 5){
+                break;
+            }
+        }
+        echo "</div></div>";
+    }
+}
+function news2()
+{
+
+    echo "<div class=\"news_container\"><div class=\"news\">";
+    $fileList = glob('aktuality/*.md');
+    foreach ($fileList as $file) {
+        $handle = fopen($file, "r");
+        $delete = true;
+        $line = fgets($handle);
+        $date = explode("-", $line);
+        $year = $date[0];
+        $mounth = $date[1];
+        $day = $date[2];
+        if ($year > date('Y', time())) {
+            $delete = false;
+        } else {
+            if ($year = date('Y', time())) {
+                if ($mounth > date('m', time())) {
+                    $delete = false;
+                }
+                if ($mounth = date('m', time())) {
+                    if ($day >= date('d', time())) {
+                        $delete = false;
+                    }
+                }
+            }
+        }
+
+        if ($delete == true) {
+            unlink($file);
+        } else {
+            echo "<h1>" . substr(str_replace(".md", "", $file), 10) . "</h1>";
+            while (($line = fgets($handle)) !== false) {
+                $i = 0;
+                str_replace('**', "<b>", $line, $count);
+                while ($i != $count) {
+                    $line = preg_replace('/\*\*/', "<b>", $line, 1);
+                    $line = preg_replace('/\*\*/', "</b>", $line, 1);
+                    $i += 2;
+                }
+                echo $line . "<br>";
+            }
+        }
+        fclose($handle);
+    }
+    echo "</div></div>";
+}
