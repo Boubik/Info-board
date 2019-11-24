66c64d29723ccfe13a3deb9437a9c49fdb8139df
Jakub <wishongames@gmail.com>
2019-11-24 21:50:08 +0100

    Highlighting active hours based on hours from config

--- index.php
+++ index.php
@@ -58,67 +58,115 @@ function getTime() {
         
 
         
-        if(hour == 7 && minute < 45 ) {
+        var h0 = $('.zero .hodina_cislo');
+        var h0t = h0.text();
+        var h0s = h0t.split(/\D/);
+
+        var h1 = $('.one .hodina_cislo');
+        var h1t = h1.text();
+        var h1s = h1t.split(/\D/);
+
+        var h2 = $('.two .hodina_cislo');
+        var h2t = h2.text();
+        var h2s = h2t.split(/\D/);
+
+        var h3 = $('.three .hodina_cislo');
+        var h3t = h3.text();
+        var h3s = h3t.split(/\D/);
+
+        var h4 = $('.four .hodina_cislo');
+        var h4t = h4.text();
+        var h4s = h4t.split(/\D/);
+
+        var h5 = $('.five .hodina_cislo');
+        var h5t = h5.text();
+        var h5s = h5t.split(/\D/);
+
+        var h6 = $('.six .hodina_cislo');
+        var h6t = h6.text();
+        var h6s = h6t.split(/\D/);
+
+        var h7 = $('.seven .hodina_cislo');
+        var h7t = h7.text();
+        var h7s = h7t.split(/\D/);
+
+        var h8 = $('.eight .hodina_cislo');
+        var h8t = h8.text();
+        var h8s = h8t.split(/\D/);
+
+        var h9 = $('.nine .hodina_cislo');
+        var h9t = h9.text();
+        var h9s = h9t.split(/\D/);
+
+        var h10 = $('.ten .hodina_cislo');
+        var h10t = h10.text();
+        var h10s = h10t.split(/\D/);
+
+        
+        
+
+        
+        if(hour == h0s[0] && minute >= h0s[2]) {
         $( ".zero" ).addClass( "active" ); 
         }else{
             $(".zero").removeClass("active");
         }
           
-        if(hour == 7 && minute >= 45 || hour == 8 && minute < 50 ) {
+        if(hour == h1s[0] && minute >=  h1s[2] || hour == h1s[4] && minute <  h1s[5] ) {
         $( ".one" ).addClass( "active" ); 
         }else{
             $(".one").removeClass("active");
         }
           
-        if(hour == 8 && minute >= 50 || hour == 9 && minute < 45 ) {
+        if(hour == h2s[0] && minute >=  h2s[2] || hour == h2s[4] && minute <  h2s[5] ) {
         $( ".two" ).addClass( "active" ); 
         }else{
             $(".two").removeClass("active");
         }
           
-        if(hour == 9 && minute >= 45 || hour == 10 && minute < 50 ) {
+        if(hour == h3s[0] && minute >= h3s[2] || hour == h3s[4] && minute < h3s[5] ) {
         $( ".three" ).addClass( "active" ); 
         }else{
             $(".three").removeClass("active");
         }
           
-        if(hour == 10 && minute >= 50 || hour == 11 && minute < 45 ) {
+        if(hour == h4s[0] && minute >= h4s[2] || hour == h4s[4] && minute < h4s[5] ) {
         $( ".four" ).addClass( "active" ); 
         }else{
             $(".four").removeClass("active");
         }
           
-        if(hour == 11 && minute >= 45 || hour == 12 && minute < 40 ) {
+        if(hour == h5s[0] && minute >= h5s[2] || hour == h5s[4] && minute < h5s[5] ) {
         $( ".five" ).addClass( "active" ); 
         }else{
             $(".five").removeClass("active");
         }
           
-        if(hour == 12 && minute >= 40 || hour == 13 && minute < 30 ) {
+        if(hour == h6s[0] && minute >= h6s[2] || hour == h6s[4] && minute < h6s[5] ) {
         $( ".six" ).addClass( "active" ); 
         }else{
             $(".six").removeClass("active");
         }
           
-        if(hour == 13 && minute >= 30 || hour == 14 && minute < 20 ) {
+        if(hour == h7s[0] && minute >= h7s[2] || hour == h7s[4] && minute < h7s[5] ) {
         $( ".seven" ).addClass( "active" );
         }else{
             $(".seven").removeClass("active");
         }
           
-        if(hour == 14 && minute >= 20 || hour == 15 && minute < 15 ) {
+        if(hour == h8s[0] && minute >= h8s[2] || hour == h8s[4] && minute < h8s[5] ) {
         $( ".eight" ).addClass( "active" ); 
         }else{
             $(".eight").removeClass("active");
         }
           
-        if(hour == 15 && minute >= 15 || hour == 16 && minute < 60 ) {
+        if(hour == h9s[0] && minute >= h9s[2] || hour == h9s[4] && minute < h9s[5] ) {
         $( ".nine" ).addClass( "active" ); 
         }else{
             $(".nine").removeClass("active");
         }
           
-        if(hour == 17 && minute <= 45 ) {
+        if(hour == h10s[0] && minute <= h10s[2] ) {
         $( ".ten" ).addClass( "active" ); 
         }else{
             $(".ten").removeClass("active");
